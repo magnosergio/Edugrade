19c8e36fa04315b8943ce2bb60e5912094a92dc5
Sérgio Magno <57957273+magnosergio@users.noreply.github.com>
2023-10-15 20:06:49 +0000

    Método RetrieveUpdateDestroy

--- alunos/urls.py
+++ alunos/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import AlunoAPIView, DisciplinaAPIView, Nota_BimestralAPIView
+from .views import AlunoAPIView, AlunosAPIView, DisciplinaAPIView, DisciplinasAPIView, Nota_BimestralAPIView, Notas_BimestraisAPIView
 
 urlpatterns = [
-    path('alunos/', AlunoAPIView.as_view(), name='alunos'),
-    path('disciplinas/', DisciplinaAPIView.as_view(), name='disciplinas'),
-    path('notas/', Nota_BimestralAPIView.as_view(), name='notas'),
+    path('alunos/', AlunosAPIView.as_view(), name='alunos'),
+    path('alunos/<int:pk>/', AlunoAPIView.as_view(), name='aluno'),
+    path('disciplinas/', DisciplinasAPIView.as_view(), name='disciplinas'),
+    path('disciplinas/<int:pk>/', DisciplinaAPIView.as_view(), name='disciplina'),
+    path('notas/', Notas_BimestraisAPIView.as_view(), name='notas'),
+    path('notas/<int:pk>/', Nota_BimestralAPIView.as_view(), name='nota'),
 ]
--- alunos/views.py
+++ alunos/views.py
@@ -3,14 +3,28 @@ from rest_framework import generics
 from .models import Aluno, Disciplina, Nota_Bimestral
 from .serializers import AlunoSerializer, DisciplinaSerializer, Nota_BimestralSerializer
 
-class AlunoAPIView(generics.ListCreateAPIView):
+class AlunosAPIView(generics.ListCreateAPIView):
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
 
-class DisciplinaAPIView(generics.ListCreateAPIView):
+class AlunoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
+class DisciplinasAPIView(generics.ListCreateAPIView):
     queryset = Disciplina.objects.all()
     serializer_class = DisciplinaSerializer
 
-class Nota_BimestralAPIView(generics.ListCreateAPIView):
+class DisciplinaAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Disciplina.objects.all()
+    serializer_class = DisciplinaSerializer
+
+
+class Notas_BimestraisAPIView(generics.ListCreateAPIView):
+    queryset = Nota_Bimestral.objects.all()
+    serializer_class = Nota_BimestralSerializer
+
+class Nota_BimestralAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Nota_Bimestral.objects.all()
     serializer_class = Nota_BimestralSerializer
